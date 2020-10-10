20d79df66c21c228540e317e513770d98b6dbc9e
nickupx <nick.upsky@gmail.com>
2020-10-10 22:47:10 +0300

    2 done + fix

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import './js/math'
+import './js/buffer'
--- src/js/__tests__/math.test.js
+++ src/js/__tests__/math.test.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-expressions */
 import { Magician, Daemon } from '../math'
 
 const gandalf = new Magician('Gandalf')
@@ -10,8 +9,13 @@ test('should get attack', () => {
 })
 
 test('should set stoned', () => {
-  vasya.stoned
-  expect(vasya.attack).toBe(48)
+  vasya.stoned = true
+  expect(vasya.stoned).toBeTruthy()
+})
+
+test('should get decreased attack', () => {
+  vasya.stoned = true
+  expect(vasya.attack).toBe(41)
 })
 
 test('should set attack', () => {
